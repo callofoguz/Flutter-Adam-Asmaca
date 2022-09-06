0bcbc13e03a254ea80ef705a1bbbb781cca1af35
Oğuzhan Kıyar <callofoguzhan@gmail.com>
2022-09-06 14:38:38 +0300

    Adam Asmaca Flutter

---
+++ Adam Asmaca Flutter/flutter_application_1/lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/colors.dart';
+import 'package:flutter_application_1/ui/widget/figure_image.dart';
+import 'package:flutter_application_1/ui/widget/letter.dart';
+import 'package:flutter_application_1/utils/game.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  //Oyundaki Kelimeyi Seçtiğimiz yer
+  String word = "Emirhan".toUpperCase();
+  // Burada alfabeyi içeren bir liste oluşturdum.
+  List<String> alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      appBar: AppBar(
+        title: Text("Adam Asmaca"),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: AppColor.primaryColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                //resimleri assets klasöründen ekledik
+                //Şimdi deneme sayısına göre şekil oluşturulacak.
+                figureImage(Game.tries >= 0, "assets/hang.png"),
+                figureImage(Game.tries >= 1, "assets/head.png"),
+                figureImage(Game.tries >= 2, "assets/body.png"),
+                figureImage(Game.tries >= 3, "assets/ra.png"),
+                figureImage(Game.tries >= 4, "assets/la.png"),
+                figureImage(Game.tries >= 5, "assets/rl.png"),
+                figureImage(Game.tries >= 6, "assets/ll.png"),
+              ],
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: word
+                .split('')
+                .map((e) => letter(e.toUpperCase(),
+                    !Game.selectedChar.contains(e.toUpperCase())))
+                .toList(),
+          ),
+
+          //Burada Oyunun klavyesini oluşturuyorum.
+          SizedBox(
+            width: double.infinity,
+            height: 250.0,
+            child: GridView.count(
+              crossAxisCount: 7,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              padding: EdgeInsets.all(8.0),
+              children: alphabets.map((e) {
+                return RawMaterialButton(
+                  onPressed: Game.selectedChar.contains(e)
+                      ? null // Butonu daha önce seçip seçmediğimizi kontrol ediyoruz.
+                      : () {
+                          setState(() {
+                            Game.selectedChar.add(e);
+                            print(Game.selectedChar);
+                            if (!word.split('').contains(e.toUpperCase())) {
+                              Game.tries++;
+                            }
+                          });
+                        },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Text(
+                    e,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  fillColor: Game.selectedChar.contains(e)
+                      ? Colors.black87
+                      : Colors.blue,
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
